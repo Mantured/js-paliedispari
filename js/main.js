3a1c77efda6f3cce4591487f8bda43bd2757a1ec
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-03 15:18:26 +0100

    static code works

--- js/main.js
+++ js/main.js
@@ -11,5 +11,18 @@
             -->forse se: se le lettere che compongono la parola sono dispari, cambia solo la lettera centrale,
             mentre se sono pari le lettere sono simmetircamente o a specchio uguali. poi vediamo
 
+            -->oppure la prendo la parola la divido con split la rigiro e dopo di che la ricompongo per controllare che sia uguale alla parola (valore) di partenza
+
 */
+const userWord = prompt(('Inserisci una parola qualsiasi')).toLowerCase().trim();
+const palindrome = userWord.split('').reverse().join('');
+console.log(palindrome);
+console.log(userWord);
+
+if (userWord === palindrome) {
+    console.log('is a palindrome');
+} else {
+    console.log('is not');
+}
+
 
