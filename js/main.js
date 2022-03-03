f78931bf397fc8fb476b69d6c74384e7889198b0
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-03 13:43:51 +0100

    new repo created - es palindorma only txt

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+    A) Chiediamo all'utente di inserire una parola
+        -- con un prompt o un imput su html DOM
+    B) Creare una funzione per stabnilire se la parola inseritra è palindroma
+        --prima bisogna stabilire le caratteristiche delle parole palindrome
+
+            -->creiamo un arrey di possobili parole palindomre?
+
+            -->c'è un modo per stabilire se una parola ANNA è != da GINO
+
+            -->forse se: se le lettere che compongono la parola sono dispari, cambia solo la lettera centrale,
+            mentre se sono pari le lettere sono simmetircamente o a specchio uguali. poi vediamo
+
+*/
+
